92dc8b39a7de0c12382e9df1d04e8730539c5042
umaraadil <55868645+umaraadil@users.noreply.github.com>
2023-10-20 22:55:05 +0530

    removed unwanted line of code

--- Program.cs
+++ Program.cs
@@ -9,39 +9,11 @@ using System.Threading.Tasks;
 
 namespace WebApp
 {
-    //public class Program
-    //{
-    //    public static void Main(string[] args)
-    //    {
-    //        CreateHostBuilder(args).Build().Run();
-    //    }
-
-    //    public static IHostBuilder CreateHostBuilder(string[] args) =>
-    //        Host.CreateDefaultBuilder(args)
-    //            .ConfigureWebHostDefaults(webBuilder =>
-    //            {
-    //                webBuilder.UseStartup<Startup>();
-    //            });
-    //}
     public class Program
     {
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            //using var scope = host.Services.CreateScope();
-            //var services = scope.ServiceProvider;
-            //var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-            //try
-            //{
-            //    var context = services.GetRequiredService<StoreContext>();
-            //    await context.Database.MigrateAsync();
-            //    await StoreContextSeed.SeedAsync(context, loggerFactory);
-            //}
-            //catch (Exception ex)
-            //{
-            //    var logger = loggerFactory.CreateLogger<Program>();
-            //    logger.LogError(ex, "An error occurred during migration");
-            //}
 
             host.Run();
         }
